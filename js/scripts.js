e189e9d179a05b64721660f43df9585778280172
Pawel Morka <morka.paul@gmail.com>
2018-01-27 11:57:20 +0100

    Add comment

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// To jest komentarz liniowy
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name); 
+
+/*
+var variable = 'test';
+console.log(variable); 
+*/
